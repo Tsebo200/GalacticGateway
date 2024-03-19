abbc813f415fd79a5b4bc68d0aa0e37644e24e83
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2024-03-20 00:19:09 +0200

    Carousel in Dashboard functional
    Make it auto slide

--- client/src/Components/Carousel.jsx
+++ client/src/Components/Carousel.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from '../Components/NavBar';
-import Carousel from '../Components/Carousel';
-
+import '../Components/Carousel.css';
 function Carousel() {
   return (
     <>
@@ -10,17 +9,59 @@ function Carousel() {
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
   </div>
 
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="..."/>
+  <div class="carousel-item active">
+    {/* Earth */}
+      <img src="https://science.nasa.gov/wp-content/uploads/2023/06/rotating_earth.gif?w=2560&format=webp&crop=1" class="d-block w-100" alt="..."/>
     </div>
+
+{/* Venus */}
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."/>
+      <img src="https://science.nasa.gov/wp-content/uploads/2016/05/venus2-jpg.webp?w=2560&format=webp&crop=1" class="d-block w-100" alt="..."/>
     </div>
+ 
+{/* Mercury */}
+  <div class="carousel-item">
+      <img src="https://science.nasa.gov/wp-content/uploads/2023/04/EW0108829708G4release_mercury-jpg.webp?w=2560&format=webp&crop=1" class="d-block w-100" alt="..."/>
+    </div>
+
+{/* Mars */}
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."/>
+      <img src="https://science.nasa.gov/wp-content/uploads/2023/04/hubble-mars-litho-image-jpg.webp?w=2560&format=webp&crop=1" class="d-block w-100" alt="..."/>
+    </div>
+
+{/* Jupiter */}
+    <div class="carousel-item">
+      <img src="https://science.nasa.gov/wp-content/uploads/2023/06/Go_-_Jupiter.png?w=2560&format=webp&crop=1" class="d-block w-100" alt="..."/>
+    </div>
+
+{/* Saturn */}
+    <div class="carousel-item">
+      <img src="https://science.nasa.gov/wp-content/uploads/2023/06/inner_rings_around_saturn_fading.gif?w=2560&format=webp&crop=1" class="d-block w-100" alt="..."/>
+    </div>
+
+{/* Uranus */}
+<div class="carousel-item">
+      <img src="https://science.nasa.gov/wp-content/uploads/2016/05/uranus-jpg.webp?w=2560&format=webp&crop=1" class="d-block w-100" alt="..."/>
+    </div>
+
+{/* Neptune */}
+<div class="carousel-item">
+      <img src="https://science.nasa.gov/wp-content/uploads/2016/05/neptune-jpg.webp?w=2560&format=webp&crop=1" class="d-block w-100" alt="..."/>
+    </div>
+
+
+{/* Pluto */}
+<div class="carousel-item">
+      <img src="https://science.nasa.gov/wp-content/uploads/2023/06/Color_enhanced_image_of_Pluto_that_shows_a_heart_shape-jpeg.webp?w=2560&format=webp&crop=1" class="d-block w-100" alt="..."/>
     </div>
   </div>
 
--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
 import Carousel from '../Components/Carousel'
-
+import styles from '../Pages/Dashboard.module.scss'
 function Dashboard() {
   return (
     <>
     <NavBar/>
-    <Carousel/>
+    <div className={styles.container}><Carousel/></div>
     <div> Dashboard </div>
     </>
   )
